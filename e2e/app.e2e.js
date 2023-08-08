a293607aac7de1f02f08f394d229a655cdba1cdf
fabCastillo <fabianboc960@hotmail.com>
2023-08-08 15:15:55 -0500

    Agregando testing

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const createApp = require('../src/app');
+const { config } = require('../src/config/config');
 
 describe('tests for app', () => {
   let app = null;
@@ -18,6 +19,36 @@ describe('tests for app', () => {
     expect(response.statusCode).toEqual(200);
     expect(response.body.name).toEqual('nico');
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  describe('GET /nueva-ruta', () => {
+    test('should return 401', async () => {
+      // Arrage
+      // Act
+      const { statusCode } = await api.get('/nueva-ruta');
+      // Assert
+      expect(statusCode).toBe(401);
+    });
+    test('should return 401 with invalid apiKey', async () => {
+      // Arrage
+      const apiKey = '123123';
+      // Act
+      const { statusCode } = await api.get('/nueva-ruta').set({
+        api: apiKey,
+      });
+      // Assert
+      expect(statusCode).toEqual(401);
+    });
+    test('should return 200', async () => {
+      // Arrage
+      const { apiKey } = config;
+      // Act
+      const { statusCode } = await api.get('/nueva-ruta').set({
+        api: apiKey,
+      });
+      // Assert
+      expect(statusCode).toEqual(200);
+    });
   });
 
   afterAll(() => {
---
+++ e2e/profile.e2e.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+const createApp = require('../src/app');
+const { models } = require('../src/db/sequelize');
+
+describe('tests for /profile path', () => {
+  let app = null;
+  let server = null;
+  let api = null;
+  let accessToken = null;
+
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(9000);
+    api = request(app);
+  });
+
+  describe('POST /my-user', () => {
+    beforeAll(async () => {
+      const user = await models.User.findByPk('1');
+      const inputData = {
+        // @ts-ignore
+        email: user.email,
+        password: 'admin123',
+      };
+      const { body } = await api.post('/api/v1/auth/login').send(inputData);
+      accessToken = body.accessToken;
+    });
+    test('should return a 401', async () => {
+      // Arrange
+      // Act
+      const { statusCode } = await api.get('/api/v1/profile/my-user').set({
+        Authorization: 'Bearer 123123',
+      });
+      // Assert
+      expect(statusCode).toBe(401);
+    });
+    test('should return a user with access token valid', async () => {
+      // Arrange
+      const user = await models.User.findByPk('1');
+      // Act
+      const { statusCode, body } = await api.get('/api/v1/profile/my-user').set({
+        Authorization: `Bearer ${accessToken}`,
+      });
+      // Assert
+      expect(statusCode).toEqual(200);
+      // check DB
+      // @ts-ignore
+      expect(body.email).toEqual(user.email);
+    });
+    afterAll(() => {
+      accessToken = null;
+    });
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+});
